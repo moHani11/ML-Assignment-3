0cd45022c83e740965a403a01764b882bf663833
moHani11 <mohamedhani004@gamil.com>
2025-12-07 09:14:52 +0200

    Add prior calculations

--- PartA/GaussianPredictor.py
+++ PartA/GaussianPredictor.py
@@ -12,12 +12,23 @@ class GaussianDigitClassifier:
         self.CovarianceMatrix = np.zeros((num_of_features, num_of_features))
         self.priors = np.zeros(num_of_classes)
 
-    def calculateClassPriors(self, y):
+    def calculateClassPriors(self, y, verbose = True):
         total_num_of_smaples = np.size(y)
+        
+        for i in range(len(self.priors)):
+            class_labels = np.where(y==i)
+            num_of_samples = np.size(class_labels)
+            self.priors[i] = num_of_samples/total_num_of_smaples
+            if verbose:
+                print(f"number of {i} samples: {num_of_samples} with prior probability: {self.priors[i]}")
+        # print(self.priors.sum())
+        if verbose:
+            print("\n\n=========================\n\n")
+
+    def calculateMeans(self, x, verbose = True):
+        
 
 
 
-
-
-
-
+if __name__ == '__main__':
+    print(np.size([True, False]))
